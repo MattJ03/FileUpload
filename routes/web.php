dfadeb96178e498724d86454e87c1b6e733961ec
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-11-28 00:39:20 +0000

    saving routes while charging

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers;
+use \App\Http\Controllers\FileController
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/file', [FileController::class, 'showFile'])->name('file.show');
+Route::post('/file', [FileController::class, 'storeFile'])->name('file.store');
+Route::
